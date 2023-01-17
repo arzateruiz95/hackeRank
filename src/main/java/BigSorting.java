a754b09b4a16906b8cc3eb92a47f64dbd73cc5cb
luis arzate <arzateruiz95@gmail.com>
2023-01-16 23:06:43 -0600

    Resolve the problem Encryption

--- src/main/java/BigSorting.java
+++ src/main/java/BigSorting.java
@@ -34,3 +34,31 @@ public class BigSorting {
 
 
 }
+/**
+ * import java.io.*;
+ * import java.util.Arrays;
+ * import java.util.Comparator;
+ *
+ * public class Solution {
+ *
+ * 	static class strcmp implements Comparator<String> {
+ *
+ *                @Override
+ *        public int compare(String o1, String o2) {
+ * 			int l1 = o1.length(), l2 = o2.length();
+ * 			return l1 > l2 ? 1 : (l2 > l1 ? -1 : o1.compareTo(o2));
+ *        }
+ *    * 	}
+ *
+ * 	public static void main(String[] args) throws IOException {
+ * 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+ * 		int n = Integer.parseInt(in.readLine());
+ * 		String[] unsorted = new String[n];
+ * 		for (int unsorted_i = 0; unsorted_i < n; unsorted_i++) {
+ * 			unsorted[unsorted_i] = in.readLine();
+ *        }
+ * 		Arrays.sort(unsorted, new strcmp());
+ * 		for(String s : unsorted)
+ * 			System.out.println(s);    * 	}
+ * }
+ */
